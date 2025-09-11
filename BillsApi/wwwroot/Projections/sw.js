2f7bb40db91da8cd7a936c203f9920bfe7cc8d80
Jerrod Cornejo <jerrodac15@yahoo.com>
2025-09-10 21:00:10 -0700

    Moved Financial Projection application into its own directory to allow bills app to function as a separate web app

--- BillsApi/wwwroot/Projections/sw.js
+++ BillsApi/wwwroot/Projections/sw.js
@@ -1,14 +1,13 @@
-const CACHE_NAME = 'financial-projection-cache-v1';
+const CACHE_NAME = 'financial-projection-cache-v2';
 const urlsToCache = [
-  '/',
-  '/FinancialProjection.html',
+  '/Projections/FinancialProjection.html',
   'https://cdn.jsdelivr.net/npm/luxon/build/global/luxon.min.js',
   'https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js',
   'https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon/dist/chartjs-adapter-luxon.min.js',
   'https://cdn.jsdelivr.net/npm/chartjs-plugin-trendline/dist/chartjs-plugin-trendline.min.js',
   'https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation@2.2.1/dist/chartjs-plugin-annotation.min.js',
-  '/android-chrome-192x192.png',
-  '/android-chrome-512x512.png'
+  '/Projections/android-chrome-192x192.png',
+  '/Projections/android-chrome-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
