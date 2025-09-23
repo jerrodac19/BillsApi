8cfadf98ff4fc7c07af4eff0a97851534e4f4c24
Jerrod Cornejo <jerrodac15@yahoo.com>
2025-09-22 21:02:10 -0700

    Revert "test comment"
    This reverts commit aabff41301bc4e9202b51b938cb14ff097796be9.

--- BillsApi/Program.cs
+++ BillsApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container. //test comment
+// Add services to the container.
 
 builder.Services.AddControllers();
 
