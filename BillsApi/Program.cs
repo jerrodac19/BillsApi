aabff41301bc4e9202b51b938cb14ff097796be9
Jerrod Cornejo <jerrodac15@yahoo.com>
2025-09-22 20:59:53 -0700

    test comment

--- BillsApi/Program.cs
+++ BillsApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. //test comment
 
 builder.Services.AddControllers();
 
